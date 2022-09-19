86a44ed67398a29094b4f00e2393b9516f733d00
Jaume <ballestertomasj@gmail.com>
2022-09-19 13:36:44 +0200

    Update App.js
    add info and button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
         <h2 className="center">BMI Calculator</h2>
         <form>
           <div>
+            <label>Height (kg)</label>
+            <input value={height} />
+          </div>
+          <div>
             <label>Weight (cm)</label>
+            <input value={weight} />
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
           </div>
         </form>
       </div>
